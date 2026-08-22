bd12b6b680547c8a40530433b9d60b778b087d68
이성희 <the.paper@live.co.kr>
2026-08-22 22:40:04 +0900

    게임 러프하게 구성

---
+++ Assets/Scripts/UITouchArea.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UITouchArea : MonoBehaviour, IPointerDownHandler
+{
+    void IPointerDownHandler.OnPointerDown(PointerEventData eventData)
+    {
+    }
+}
